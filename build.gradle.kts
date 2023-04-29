a85be4b134b1ccb5f2995dd911574f07df131c3c
Pierre-Yves Fourmond <33195569+grumpyf0x48@users.noreply.github.com>
2023-04-29 18:14:18 +0200

    Use Gradle Kotlin DSL (#31)
    * Use Gradle Kotlin DSL
    
    * Use Gradle Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    application
+    java
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+version = "0.1-SNAPSHOT"
+
+val springBootPluginVersion: String by project
+val picocliSpringBootVersion: String by project
+val lombokVersion: String by project
+val junitVersion: String by project
+val mockitoVersion: String by project
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter:${springBootPluginVersion}")
+    implementation("info.picocli:picocli-spring-boot-starter:${picocliSpringBootVersion}")
+    implementation("org.projectlombok:lombok:${lombokVersion}")
+    annotationProcessor("org.projectlombok:lombok:${lombokVersion}")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:${springBootPluginVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter:${junitVersion}")
+    testImplementation("org.mockito:mockito-core:${mockitoVersion}")
+}
+
+application {
+    mainClass.set("org.grumpyf0x48.applicationName.Application")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+repositories {
+    mavenCentral()
+}

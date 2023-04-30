ebb91a00cf1cebfdccb394b3ef29009d61fa7933
Pierre-Yves Fourmond <33195569+grumpyf0x48@users.noreply.github.com>
2023-04-30 11:40:24 +0200

    Add native distribution (#37)
    * Add native distribution
    
    * Add group
    
    * Disable native build for automatic builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,10 @@ plugins {
     java
     id("org.springframework.boot")
     id("io.spring.dependency-management")
+    id("org.graalvm.buildtools.native")
 }
 
+group = "org.grumpyf0x48"
 version = "0.1-SNAPSHOT"
 
 val springBootPluginVersion: String by project
@@ -43,6 +45,18 @@ distributions {
             }
         }
     }
+    create("native") {
+        contents {
+            from("${project.buildDir}/generated") {
+                into("completion")
+                include("myapplication")
+            }
+            from("${project.buildDir}/native/nativeCompile") {
+                into("bin")
+                include("myapplication")
+            }
+        }
+    }
 }
 
 val generateCompletion = task("generateCompletion", JavaExec::class) {
@@ -59,6 +73,33 @@ tasks.distZip {
 
 tasks.distTar {
     dependsOn(generateCompletion)
+}
+
+tasks.getByName("nativeDistZip") {
+    dependsOn(generateCompletion)
+    dependsOn(tasks.nativeCompile)
+}
+
+tasks.getByName("nativeDistTar") {
+    dependsOn(generateCompletion)
+    dependsOn(tasks.nativeCompile)
+}
+
+tasks.getByName("installNativeDist") {
+    dependsOn(generateCompletion)
+    dependsOn(tasks.nativeCompile)
+}
+
+graalvmNative {
+    toolchainDetection.set(false)
+    binaries {
+        all {
+            resources.autodetect()
+        }
+        named("main") {
+            imageName.set("myapplication")
+        }
+    }
 }
 
 tasks.withType<Test> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,21 @@
 pluginManagement {
     val springBootPluginId: String by settings
     val springDependencyPluginId: String by settings
+    val nativeImagePluginId: String by settings
 
     val springBootPluginVersion: String by settings
     val springDependencyPluginVersion: String by settings
+    val nativeImagePluginVersion: String by settings
 
     repositories {
         mavenCentral()
+        gradlePluginPortal()
     }
 
     plugins {
         id(springBootPluginId) version springBootPluginVersion
         id(springDependencyPluginId) version springDependencyPluginVersion
+        id(nativeImagePluginId) version nativeImagePluginVersion
     }
 }
 

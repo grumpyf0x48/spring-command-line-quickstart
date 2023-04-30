b1791b3533eb46f97524e0ff19a0b5426e2f3e95
Pierre-Yves Fourmond <33195569+grumpyf0x48@users.noreply.github.com>
2023-04-30 09:40:25 +0200

    Add Bash completion script (#35)
    * Add Bash completion script
    
    * Call distZip explicitly

---
+++ src/main/java/org/grumpyf0x48/myapplication/commands/Completion.java
@@ -0,0 +1,13 @@
+package org.grumpyf0x48.myapplication.commands;
+
+// This class is processed by Picocli Codegen
+// which needs a no-args constructor in Command class
+// to generate Bash completion script
+// That cannot be done in super class because of Spring dependency injection
+
+public class Completion extends Command {
+
+    public Completion() {
+        super(null);
+    }
+}

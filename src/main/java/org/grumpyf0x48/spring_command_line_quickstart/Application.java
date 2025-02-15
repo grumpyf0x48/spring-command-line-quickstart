c152fb79189ae36b8bc8be741a5208873b31e712
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2025-02-15 11:04:56 +0100

    Démarrer l'app Spring Boot comme on a l'habitude de le faire avec la méthode run()

--- src/main/java/org/grumpyf0x48/spring_command_line_quickstart/Application.java
+++ src/main/java/org/grumpyf0x48/spring_command_line_quickstart/Application.java
@@ -4,18 +4,19 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(Application.class)
-            .logStartupInfo(false)
-            .bannerMode(Banner.Mode.OFF)
-            .web(WebApplicationType.NONE)
-            .run(args);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.setLogStartupInfo(false);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.setWebApplicationType(WebApplicationType.NONE);
+
+        ConfigurableApplicationContext applicationContext = springApplication.run(args);
+
         int exitCode = SpringApplication.exit(applicationContext);
         System.exit(exitCode);
     }
